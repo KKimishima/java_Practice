be68f5be9ae28cc362b3aabf0e00f80a58ef04c7
君島克弥 <K.Kimishima123@gmail.com>
2017-10-21 19:19:58 +0900

    DBデータ挿入

--- 12.DB/p1201/IntoRs.java
+++ 12.DB/p1201/IntoRs.java
@@ -1,49 +1,17 @@
-
-import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class CreatTb extends DBConnet{
+public class IntoRs extends DBConnet{
   private String tb;
-  private ArrayList<String> colum;
+  private ArrayList<String> dataset;
 
-  public CreatTb(String host, String db, String user, String pass,String tb){
-    super(host,db,user,pass);
+  // コンストラクタ
+  public IntoRs(String host, String db, String user, String pass,String tb) {
+    super(host, db, user, pass);
     this.tb = tb;
   }
 
-  // Sql
-  @Override
-  public void setSql(String tb){
-    Connection con  = null;
-    try{
-      con = DriverManager.getConnection(
-          "jdbc:mariadb://"+super.getHost()+":"+super.getPort()+"/"+super.getDb(),
-          super.getUser(),super.getPass());
-      // sqlの雛形を作製
-      PreparedStatement cre = con.prepareStatement(
-          "create table "+this.tb+"("+colum.get(0)+" varchar(10),"+colum.get(1)+" int,"+colum.get(2)+" int);"
-      );
-      // sqlに流し込み
-      // sql実行
-      cre.executeUpdate();
-      con.close();
-    }catch (SQLException e2){
-      System.out.println("SQL実行失敗");
-      e2.printStackTrace();
-      System.exit(2);
-    }
-  }
-
-  // セットカラム
-  @Override
-  public void setCloum(String x,String y,String z){
-    colum = new ArrayList();
-    colum.add(x);
-    colum.add(y);
-    colum.add(z);
-  }
-  // テーブルのチェック
+  // テーブルチェック
   @Override
   public  boolean checkTable(){
     Connection con  = null;
@@ -62,14 +30,45 @@ public class CreatTb extends DBConnet{
       // 成功失敗判別
       if (r.next()) {
         con.close();
-        return true;
+        return false;
       }
+    }catch (SQLException e2){
+      System.out.println("SQL実行失敗");
+      e2.printStackTrace();
+      System.exit(2);
+    }
+    return true;
+  }
+
+  @Override
+  public void setCloum(String x,String y,String z){
+    dataset = new ArrayList<>();
+    dataset.add(x);
+    dataset.add(y);
+    dataset.add(y);
+  }
+
+  @Override
+  public void setSql(String tb){
+    Connection con  = null;
+    try{
+      con = DriverManager.getConnection(
+          "jdbc:mariadb://"+super.getHost()+":"+super.getPort()+"/"+super.getDb(),
+          super.getUser(),super.getPass());
+      // sqlの雛形を作製
+      PreparedStatement cre = con.prepareStatement(
+          "insert into "+this.tb+" values("+dataset.get(0)+"999,999)"
+          "in table "+this.tb+"("+colum.get(0)+" varchar(10),"+colum.get(1)+" int,"+colum.get(2)+" int);"
+      );
+      // sqlに流し込み
+      // sql実行
+      cre.executeUpdate();
       con.close();
     }catch (SQLException e2){
       System.out.println("SQL実行失敗");
       e2.printStackTrace();
       System.exit(2);
     }
-    return false;
   }
+
 }
--- 12.DB/p1201/Main.java
+++ 12.DB/p1201/Main.java
@@ -4,22 +4,44 @@ import java.util.Map;
 // テーブル作製
 public class Main {
   public static void main(String[] args) {
+    String settb = "tb2";
+    String host = "localhost";
+    String db   = "db1";
+    String user = "vagrant";
+    String pass = "vagrant";
+
+
     System.out.println("処理スタート");
+
+    // 接続のチェック
     // db接続テスト
     DBConnet dbcon1 = new CreatTb(
-        "192.168.33.11","testdb1","vagrant","vagrant","tb3");
+        host,db,user,pass,settb);
     // ドライバテスト
     dbcon1.setDriver();
-    // db接続テスト
+    // テーブルチェク
     dbcon1.conTest();
     if(dbcon1.checkTable()){
-      System.out.println("既存のテーブルがあります¥n被らない名前をつけてください");
+      System.out.println("既存のテーブルがあります。被らない名前をつけてください");
       System.exit(1);
     }
+
+    // テーブル作製
     // カラムの設定
     dbcon1.setCloum("NAME","PRICE","WEIGHT");
     // SQL操作
-    dbcon1.setSql("tb2");
-    System.out.println("処理完了");
+    dbcon1.setSql(settb);
+    System.out.println("テーブル作製完了");
+
+    // テーブルに値を入れつ
+    DBConnet dbcon2 = new IntoRs(
+    host,db,user,pass,settb);
+    // 入れるテーブルの確認
+     dbcon1.conTest();
+    if(dbcon1.checkTable()){
+      System.out.println("データを挿入するテーブルがありません!!!");
+      System.exit(1);
+    }
+    // sqlセット
   }
 }
